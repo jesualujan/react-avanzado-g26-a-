0b9fc79141fb0e7afe8d6e10bc7e4d0ebe39a85a
jesualujan <lujan.oficial@gmail.com>
2023-10-19 19:10:28 -0600

    ✨ definimos nuestras reglas de validación

--- 01-react-forms/src/Components/ReactHookForm/ReactHookForm.jsx
+++ 01-react-forms/src/Components/ReactHookForm/ReactHookForm.jsx
@@ -7,17 +7,23 @@ import {yupResolver} from '@hookform/resolvers/yup'
 const ReactHookForm = () => {
      //* NUESTRA LÓGICA | CREACIÓN DEL ESQUEMA DEL FORMULARIO DE USUARIO UTILIZANDO YUP, PARA VALIDAR NUESTROS CAMPOS
     const userFormSchema = yup.object()({ 
-        firstName: ,
-        lastName: ,
-        age: ,
-        password: ,
-        gender: ,
-
+        firstName: yup.string().required('Escribe tu nombre'),
+        lastName: yup.string().required('Escribe tu apellido'),
+        age: yup.number().positive('La edad debe ser un número positivo').integer('Ingresa tu edad'),
+        password: yup.string().required('No ingresaste una contraseña')
+                     .min(5, 'La contraseña es muy corta, debe tener al menos 5 caracteres')
+                     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%.^&*])/,  
+                     'La contraseña debe tener al menos 5 caracteres, un número, una letra mayúscula, una letra minúscula y un caracter especial'),
+        gender: yup
+                .mixed()
+                .oneOf(['M','F','O'], 'Selecciona un género: Hombre, Mujer u Otro')
+                .defined()
+                 ,
     }).required()
 
 
   return (
-    <div className='login'>
+    <div className='login'>)
       <div className='login-container'>
         <img src={logo} alt='logo' />
         <form
