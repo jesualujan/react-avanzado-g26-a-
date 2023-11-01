ee2155cb3e86471caaa055c577eae5c5b61edb76
jesualujan <lujan.oficial@gmail.com>
2023-10-31 19:44:27 -0600

    ✨ creamos nuestro nav aplicando BEM

---
+++ 03-react-auth/src/Components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import './header.scss'
+
+const Header = () => {
+  // APLICANDO BEM EN NUESTRAS className DE REACT
+  return (
+    <nav className='header'>
+      <a className='header__logo' href='/'>LOGO</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/home'>Home</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/dashboard'>Dashboard</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/secret'>Secret</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/login'>Login</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/signup'>Signup</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
