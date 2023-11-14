8abfae075bebe80dddbe40870279bfb53445c73f
jesualujan <lujan.oficial@gmail.com>
2023-11-14 16:48:12 -0600

    ✨ fix all auto-fixable

--- 03-react-auth/src/Routes/RoutesIndex.jsx
+++ 03-react-auth/src/Routes/RoutesIndex.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
 import { useAuthContext } from '@/Context/useAuthContext'
 
 const RoutesIndex = () => {
-  const {isAuth, userPayload} = useAuthContext()
+  const { isAuth, userPayload } = useAuthContext()
   console.log(isAuth, userPayload)
   return (
     <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/dashboard' element={ isAuth ? <Dashboard/> : <Login/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/secret' element={isAuth ? <Secret/> : <Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/dashboard' element={isAuth ? <Dashboard /> : <Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/secret' element={isAuth ? <Secret /> : <Login />} />
+      <Route path='/signup' element={<Signup />} />
     </Routes>
   )
 }
